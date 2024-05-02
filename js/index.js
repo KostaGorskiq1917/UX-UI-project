e0fc40c1043887a35721af653494ce92c8cfee85
Kostadin Gramatikov <kgramatikov993@gmail.com>
2024-05-02 18:48:17 +0300

    Uploading the project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function readText(element)
+{
+    const text = element.textContent;
+    const speech = new SpeechSynthesisUtterance(text);
+    speech.lang = 'en-US';
+    window.speechSynthesis.speak(speech);
+}
+
+function stopRead()
+{
+    const synth = window.speechSynthesis;
+    synth.cancel();
+}
+console.log("works");
